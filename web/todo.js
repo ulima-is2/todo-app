f3c46c79b815d2f14d14029e512d77fec798b765
Hernan <hquintana@devosinc.com>
2016-10-23 21:31:07 -0500

    Se implemento frontend

--- web/todo.js
+++ web/todo.js
@@ -1,7 +1,32 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+angular.module('todoApp', [])
+        .controller('TodoListController', function () {
+            var todoList = this;
+            todoList.todos = [
+                {text: 'Estudiar patrones', done: true},
+                {text: 'Avanzar para la entrega 4', done: false}];
 
+            todoList.addTodo = function () {
+                todoList.todos.push({
+                    text: todoList.todoText, 
+                    done: false
+                });
+                todoList.todoText = '';
+            };
 
+            todoList.remaining = function () {
+                var count = 0;
+                angular.forEach(todoList.todos, function (todo) {
+                    count += todo.done ? 0 : 1;
+                });
+                return count;
+            };
+
+            todoList.archive = function () {
+                var oldTodos = todoList.todos;
+                todoList.todos = [];
+                angular.forEach(oldTodos, function (todo) {
+                    if (!todo.done)
+                        todoList.todos.push(todo);
+                });
+            };
+        });
